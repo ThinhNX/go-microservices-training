a08705029750d9b8e2b9218ae185ee2510c59a4c
ThinhNX <xuanthinh.1411@icloud.com>
2023-03-25 23:43:45 +0700

    v3.2 fix login logic

--- services/handler/login/login.go
+++ services/handler/login/login.go
@@ -1,22 +1,26 @@
 package login
 
 import (
-	"encoding/json"
 	"go-microservices-training/services/model"
-	"net/http"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func LoginHandler(c *gin.Context) {
-	jsd := json.NewDecoder(c.Request.Body)
-	var loginBody model.User
-	err := jsd.Decode(&loginBody)
-	if err != nil {
-		c.Data(http.StatusUnauthorized, "", []byte(err.Error()))
-	}
+	// jsd := json.NewDecoder(c.Request.Body)
+	// var loginBody model.User
+	// err := jsd.Decode(&loginBody)
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// 	c.Data(http.StatusUnauthorized, "", []byte(err.Error()))
+	// }
+	queryP := c.Request.URL.Query()
+	userName := queryP.Get("user")
+	pass := queryP.Get("pass")
+	log.Println("Login User: ", userName)
 	for _, v := range model.UserList {
-		if v.Name == loginBody.Name && v.Password == loginBody.Password {
+		if v.Name == userName && v.Password == pass {
 			c.Data(200, "text/html", nil)
 		} else {
 			c.Data(401, "text/html", nil)
